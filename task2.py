62644dc09015537a283bc5806cdbd390df2ca10b
jarvis <jarvisyeh@qq.com>
2020-11-16 14:53:47 +0800

    1

--- task2.py
+++ task2.py
@@ -79,7 +79,7 @@ def perfTest(tcp_type):
     # task start here
     print "User set tcp type as : " + tcp_type + "!!!!"  
     TCP_TYPE = tcp_type
-    TIME = 100
+    TIME = 300
     h1, h3, h8 = net.get('h1', 'h3', 'h8')
 
     h8.cmd('iperf3 -s -i 1 > h8_server_%s_%d.txt &' % (TCP_TYPE, myQueueSize))
